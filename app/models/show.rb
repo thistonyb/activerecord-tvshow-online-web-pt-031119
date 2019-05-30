b85a27710ad07beb3e6f0ed31feb939be8fa46d9
Tony Baker <this.tonybaker@gmail.com>
2019-05-30 20:05:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where("rating = ?", highest_rating).take
   end
+
+  def self.lowest_rating
+    Show.minimum("rating")
+  end
 end
